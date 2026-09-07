6d26b9d54200f97dbf69cd917d184006bdcb250c
poput <159613160+poput55@users.noreply.github.com>
2026-09-07 18:29:58 +0300

    add burger menu

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,44 +1,63 @@
 package com.weatherapp
 
+import android.R
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.gson.Gson
 import com.weatherapp.data.WeatherModels
 import com.weatherapp.screens.WeatherState
 import com.weatherapp.screens.WeatherViewModel
 import com.weatherapp.ui.theme.WeatherAppTheme
+import kotlinx.coroutines.launch
+import java.time.MonthDay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,96 +80,134 @@ class MainActivity : ComponentActivity() {
 fun WeatherScreen(
     viewModel: WeatherViewModel = viewModel()
 ){
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences("weather_pref", Context.MODE_PRIVATE)
+    val savedCity = sharedPreferences.getString("savedCity","") ?: ""
+
     val weatherState by viewModel.weatherState.collectAsState()
-    var city by remember { mutableStateOf("")   }
+    var city by remember { mutableStateOf(savedCity)   }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(35.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        OutlinedTextField(
-            value = city,
-            onValueChange = { city = it} ,
-            label = {Text("Введите город (на англ)")},
-        )
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Button(
-            onClick = { viewModel.fetchWeather(city) },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Узнать погоду")
-        }
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        when (val state = weatherState) {
-            is WeatherState.Loading -> {
-                CircularProgressIndicator()
-                Text("Загрузка", modifier = Modifier.padding(top = 8.dp))
-            }
-
-            is WeatherState.Succes -> {
-                Card(
-                    modifier = Modifier.fillMaxWidth(),
-                    elevation = CardDefaults.cardElevation(4.dp)
-                ) {
-                    Column(
-                        modifier = Modifier.padding(24.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = state.data.name,
-                            style = MaterialTheme.typography.headlineMedium
-                        )
-
-                        Spacer(modifier = Modifier.height(8.dp))
-
-                        Text(
-                            text = "${state.data.main.temp.toInt()}°C",
-                            style = MaterialTheme.typography.displayLarge,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-
-                        Text(
-                            text = state.data.weather[0].description.replaceFirstChar { it.uppercase() },
-                            style = MaterialTheme.typography.bodyLarge
-                        )
-
-                        Spacer(modifier = Modifier.height(8.dp))
-
-                        Text(
-                            text = "Влажность: ${state.data.main.humidity}%",
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = MaterialTheme.colorScheme.onSurfaceVariant
-                        )
-                    }
-                }
-            }
-
-            is WeatherState.Error -> {
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(modifier = Modifier.height(10.dp))
                 Text(
-                    text = state.message,
-                    color = MaterialTheme.colorScheme.error,
-                    style = MaterialTheme.typography.bodyLarge
+                    text = "$savedCity",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
             }
+        }
+    ) {
 
-            is WeatherState.Idle ->
-            {
-                Card(
-                    modifier = Modifier.fillMaxWidth(),
-                    elevation = CardDefaults.cardElevation(4.dp)
-                ) {
-                    Text(
-                        text = "Введите город и нажмите кнопку",
-                        style = MaterialTheme.typography.headlineSmall,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        modifier = Modifier.padding(32.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(35.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.TopStart
+            ){
+                IconButton (
+                    onClick = {scope.launch { drawerState.open() }},
+                    ) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Открыть меню"
                     )
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                value = city,
+                onValueChange = { city = it} ,
+                label = {Text("Введите город (на англ)")},
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = {
+                    sharedPreferences.edit().putString("savedCity",city).apply()
+                    viewModel.fetchWeather(city) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Узнать погоду")
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            when (val state = weatherState) {
+                is WeatherState.Loading -> {
+                    CircularProgressIndicator()
+                    Text("Загрузка", modifier = Modifier.padding(top = 8.dp))
+                }
+
+                is WeatherState.Succes -> {
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        elevation = CardDefaults.cardElevation(4.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(24.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = state.data.name,
+                                style = MaterialTheme.typography.headlineMedium
+                            )
+
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                            Text(
+                                text = "${state.data.main.temp.toInt()}°C",
+                                style = MaterialTheme.typography.displayLarge,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+
+                            Text(
+                                text = state.data.weather[0].description.replaceFirstChar { it.uppercase() },
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                            Text(
+                                text = "Влажность: ${state.data.main.humidity}%",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
+                }
+
+                is WeatherState.Error -> {
+                    Text(
+                        text = state.message,
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+
+                is WeatherState.Idle ->
+                {
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        elevation = CardDefaults.cardElevation(4.dp)
+                    ) {
+                        Text(
+                            text = "Введите город и нажмите кнопку",
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier.padding(32.dp)
+                        )
+                    }
                 }
             }
         }
